82eff26efb9747b9ec55672c920de318e41c94b1
antanasbendoraitis <antanasbendoraitis7@gmail.com>
2020-12-12 19:45:29 +0200

    Add user data table.

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users/users.service';
+import { User } from '../../interfaces/user';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminUsersComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+  displayedColumns: string[] = [
+    'id',
+    'personalcode',
+    'username',
+    'firstname',
+    'lastname',
+    'email',
+    'department',
+    'registerdate',
+    'birthdate',
+    'status',
+    'workplace',
+  ];
+  constructor(private usersService:UsersService) { }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.usersService
+      .getUsers()
+      .subscribe((response) => (this.users = [...response.data]));
   }
 
 }
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+    id: number;
+    personal_code: string;
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    department: string;
+    register_date:string;
+    birthdate:string;
+    status:string;
+    workplace:string;
+  }
+  
